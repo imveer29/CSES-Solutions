97279a175f33d860f442bf98a12828bb569bdec3
Chandraveer Singh <97470373+imveer29@users.noreply.github.com>
2022-12-27 10:46:57 +0530

    Two Sets II

---
+++ cp.cpp
@@ -0,0 +1,111 @@
+/*Chandraveer Singh*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+/*-----------------------MACROS-----------------------------------*/
+
+#define fastio()         ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD              1000000007
+#define MOD1             998244353
+#define INF              1e18
+#define endl             "\n"
+#define sz(x)            (int)x.size()
+#define pb               push_back
+#define ppb              pop_back
+#define mp               make_pair
+#define ff               first
+#define ss               second
+#define pi               3.141592653589793238462
+#define all(x)           (x).begin(), (x).end()
+#define setbits          __builtin_popcount
+#define setbitsll        __builtin_popcountll
+
+/*-----------------------Debugging--------------------------------*/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update > pbds;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(unordered_set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(unordered_map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(unordered_set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+/*--------------------------Algorithms----------------------------*/
+
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+void google(int t) {cout << "Case #" << t << ": ";}
+
+/*--------------------------Code begins---------------------------*/
+
+//Problem Link - https://cses.fi/problemset/task/1093
+void solve() {
+    ll n;
+    cin >> n;
+    ll sum = (n * (n + 1)) / 2;
+    if (sum & 1) {
+        cout << 0 << endl;
+        return;
+    }
+    sum /= 2;
+    vector<vector<ll>> dp(n + 1, vector<ll>(sum + 1));
+    dp[0][0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j <= sum; ++j)
+            dp[i][j] = (dp[i - 1][j] + (j >= i ? dp[i - 1][j - i] : 0)) % MOD;
+    }
+    cout << (dp[n][sum] * 500000004) % MOD << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+    fastio();
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+}
